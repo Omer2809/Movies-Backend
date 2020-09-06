c0f27c9f2f1018e279a810ba06af2148c5b8d1f3
Mohd Omer <omer786501@gmail.com>
2020-09-06 23:14:48 +0530

    4th Commit

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,10 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function() {
-  mongoose.connect('mongodb://localhost/moviedb',{useNewUrlParser:true},{useUnifiedTopology: true}).then(() => winston.info('Connected to MongoDB...'));
+  const db = config.get('db');
+  mongoose.connect(db,{useNewUrlParser:true},{useUnifiedTopology: true}).then(() => winston.info(`Connected to ${db}...`));
   
   mongoose.set("useFindAndModify", false);
   mongoose.set("useCreateIndex", true);
