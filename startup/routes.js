a217bab5e3b7bd761775acb24b1ad2b121046bf7
Mohd Omer <omer786501@gmail.com>
2020-09-07 18:28:18 +0530

    allow access

--- startup/routes.js
+++ startup/routes.js
@@ -5,9 +5,11 @@ const favorite = require('../routes/favorite');
 const like = require('../routes/like');
 const comment = require('../routes/comment');
 const error = require('../middleware/error');
+const allowAccess = require('./allowAccess');
 
 module.exports = function(app) {
   app.use(express.json());
+  app.use(allowAccess);
   app.use('/api/users', users);
   app.use('/api/auth', auth);
   app.use('/api/favorite', favorite);
